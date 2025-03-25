11798ffbfda7b61e2a0843012dc572b21a766dfe
Sr-3pp <martin.ru@outlook.com>
2025-03-25 08:58:41 -0600

    site under construction

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Container :with-padding="true">
+      <Logo />
+      <h1>
+        Sitio en construcción
+      </h1>
+    </Container>
+  </div>
+</template>
+
+<style lang="scss">
+.container{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1{
+  text-align: center;
+}
+</style>
